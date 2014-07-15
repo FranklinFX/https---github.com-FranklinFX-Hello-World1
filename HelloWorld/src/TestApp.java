b03b444e88873381bcfea19b0e5bad85c603db3d
Franklin Xavier <franklin.xavier@capestart.com>
2014-07-15 15:26:24 +0530

    Initial Commit of HelloWorld.java

---
+++ HelloWorld/src/TestApp.java
@@ -0,0 +1,13 @@
+
+public class TestApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test App");
+
+	}
+
+}
